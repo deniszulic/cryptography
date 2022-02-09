be8d7675ce0e9e3feba82508c79886036c3ac98e
deniszulic <35138382+deniszulic@users.noreply.github.com>
2022-02-10 00:01:40 +0100

    aes_rsa

--- aes_rsa/aes_rsa.py
+++ aes_rsa/aes_rsa.py
@@ -4,19 +4,19 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 
 key = RSA.generate(2048)
 private_key = key.exportKey()
-file_out = open("aes_rsa/private.pem", "wb")
+file_out = open("private.pem", "wb")
 file_out.write(private_key)
 file_out.close()
 
 public_key = key.publickey().exportKey()
-file_out = open("aes_rsa/receiver.pem", "wb")
+file_out = open("receiver.pem", "wb")
 file_out.write(public_key)
 file_out.close()
 
 data = "Kriptografija".encode("utf-8")
 file_out = open("encrypted_data.bin", "wb")
 
-recipient_key = RSA.import_key(open("aes_rsa/receiver.pem").read())
+recipient_key = RSA.import_key(open("receiver.pem").read())
 session_key = get_random_bytes(16)
 
 cipher_rsa = PKCS1_OAEP.new(recipient_key)
@@ -29,7 +29,7 @@ file_out.close()
 
 file_in = open("encrypted_data.bin", "rb")
 
-private_key = RSA.import_key(open("aes_rsa/private.pem").read())
+private_key = RSA.import_key(open("private.pem").read())
 
 enc_session_key, nonce, tag, ciphertext = \
    [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
